274b97e730c9fc544652806ef93c30802b22b97a
JanKakol <jachu50012@gmail.com>
2018-10-24 22:36:08 +0200

    Add liquibase to junit tests.

--- src/test/java/com/jankkol/fodmap/ingredient/IngredientRestControllerTest.java
+++ src/test/java/com/jankkol/fodmap/ingredient/IngredientRestControllerTest.java
@@ -1,6 +1,14 @@
 package com.jankkol.fodmap.ingredient;
 
 import com.jankkol.fodmap.FodmapApplication;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.FileSystemResourceAccessor;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +17,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -23,6 +35,30 @@ public class IngredientRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    private static Connection conn;
+    private static Liquibase liquibase;
+
+    @BeforeClass
+    public static void createTestData() throws SQLException,
+            ClassNotFoundException, LiquibaseException {
+
+        Class.forName("org.h2.Driver");
+        conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+
+        Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(new JdbcConnection(conn));
+
+        liquibase = new Liquibase("db-schema.xml",
+                new FileSystemResourceAccessor(), database);
+        liquibase.update((String) null);
+    }
+
+    @AfterClass
+    public static void removeTestData() throws LiquibaseException, SQLException {
+        liquibase.rollback(1000, null);
+        conn.close();
+    }
 
     @Test
     public void initTest() throws Exception {
